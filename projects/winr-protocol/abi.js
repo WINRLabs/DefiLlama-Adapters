fdaabbdb1a58e662c106a3c6a72ac3403423c1e9
Anynomous <any@nomo.us>
2024-11-27 18:10:33 +0300

    winr protocol v1 lives under arbitrum. It still runs on arb

---
+++ projects/winr-protocol/abi.js
@@ -0,0 +1,18 @@
+var abi = [
+  {
+    constant: true,
+    inputs: [],
+    name: "totalSupply",
+    outputs: [
+      {
+        name: "",
+        type: "uint256",
+      },
+    ],
+    payable: false,
+    stateMutability: "view",
+    type: "function",
+  },
+];
+
+module.exports = { abi };
